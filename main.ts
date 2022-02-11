b5390782fe6dfcae5215670b270188334e4d4ab8
antoinehrghrjn <99180625+antoinehrghrjn@users.noreply.github.com>
2022-02-11 08:20:29 +0100

    Mise à jour main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,16 +1,45 @@
 input.onButtonPressed(Button.A, function () {
     radio.sendNumber(2)
+    images.createImage(`
+        . . # . .
+        . . # . .
+        # . # . #
+        . # # # .
+        . . # . .
+        `).showImage(0)
 })
 input.onGesture(Gesture.TiltRight, function () {
     radio.sendNumber(3)
+    images.createImage(`
+        . . # . .
+        . . . # .
+        # # # # #
+        . . . # .
+        . . # . .
+        `).showImage(0)
 })
 input.onGesture(Gesture.TiltLeft, function () {
     radio.sendNumber(4)
+    images.createImage(`
+        . . # . .
+        . # . . .
+        # # # # #
+        . # . . .
+        . . # . .
+        `).showImage(0)
 })
 input.onButtonPressed(Button.AB, function () {
     radio.sendNumber(5)
+    images.iconImage(IconNames.No).showImage(0)
 })
 input.onButtonPressed(Button.B, function () {
     radio.sendNumber(1)
+    images.createImage(`
+        . . # . .
+        . # # # .
+        # . # . #
+        . . # . .
+        . . # . .
+        `).showImage(0)
 })
 radio.setGroup(1)
